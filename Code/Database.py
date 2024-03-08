af11a034dae21bd4e1967ee57ac685730a554aa0
Yoithebrain <crazyone5477@gmail.com>
2024-03-08 09:42:52 +0100

    Added a small decorator log to the database class, to showcase the decorator pattern

--- Code/Database.py
+++ Code/Database.py
@@ -1,3 +1,33 @@
+###
+# Database class - makes use of a dictionary to contain the items for now
+# - CLY 030824 -
+####
+# Imports
+import logging
+
+# Logging configuration
+logging.basicConfig(filename='database.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# Logging decorator
+def log_action(func):
+    def wrapper(*args, **kwargs):
+        # Calls the original method
+        result = func(*args, **kwargs)
+        # Get the name of the action performed
+        action = func.__name__
+        # Log the action, key and value into the log file
+        if action == 'insert_data':
+            key, value = args[0], args[1]
+            logging.info(f"Item added: Key='{key}', Value='{value}'")
+        elif action == 'get_data':
+            key = args[0]
+            value = result
+            logging.info(f"Item loaded: Key='{key}', Value='{value}'")
+        elif action == 'delete_data':
+            key = args[0]
+            logging.info(f"Item deleted: Key='{key}'")
+        return result
+    return wrapper
+# Class definition
 class Database:
     _instance = None
     _data = {}
@@ -6,19 +36,22 @@ class Database:
         if cls._instance is None:
             cls._instance = super().__new__(cls)
         return cls._instance
-
-    def insert_data(self, key, value):
-        self._data[key] = value
-
-    def get_data(self, key):
-        return self._data.get(key)
-
-    def delete_data(self, key):
-        if key in self._data:
-            del self._data[key]
+    @staticmethod
+    @log_action
+    def insert_data(key, value):
+        Database._data[key] = value
+    @staticmethod
+    @log_action
+    def get_data(key):
+        return Database._data.get(key)
+    @staticmethod
+    @log_action
+    def delete_data(key):
+        if key in Database._data:
+            del Database._data[key]
 
 # Debug code - Example usage:
-'''
+
 db1 = Database()
 db1.insert_data("key1", "value1")
 
@@ -30,4 +63,3 @@ print(db1.get_data("key2"))  # Output: value2 (Both instances share the same dat
 
 db1.delete_data("key1")
 print(db2.get_data("key1"))  # Output: None (Changes made in one instance affect the other)
-'''
